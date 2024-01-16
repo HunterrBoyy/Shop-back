e836d6dec854631ac4bee105fcf0e53861002e4e
HunterrBoyy <shahiaaayus@gmail.com>
2024-01-17 00:03:48 +0545

    ch

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
-const orderRoutes = require('./routes/orderRoutes')
+const orderRoutes = require('./routes/orderRoutes');
 const mongoose = require('mongoose');
 const port = 5000;
 const cors = require('cors');
-const fileUpload = require('express-fileupload')
+const fileUpload = require('express-fileupload');
 
 
 mongoose.connect('mongodb+srv://hunterboy:ayush1998@cluster0.fxwwuen.mongodb.net/').then((result) => {
@@ -18,28 +18,19 @@ mongoose.connect('mongodb+srv://hunterboy:ayush1998@cluster0.fxwwuen.mongodb.net
   console.log(err);
 })
 
-app.use('/uploads',express.static('uploads'))
-app.use(cors()); 
+app.use('/uploads', express.static('uploads'))
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({
-  limits:{filesize:5 * 1024 * 1024},
+  limits: { fileSize: 5 * 1024 * 1024 },
   abortOnLimit: true,
-}))
+
+}));
 
 app.use(authRoutes);
 app.use(productRoutes);
 app.use(orderRoutes);
-
-
-
-
-// app.get('/', (req, res) => {
-//   return res.status(200).json({
-//     id: 1,
-//     title: 'lio'
-//   });
-// })
 
 
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllProducts, addProduct, getProduct, removeProduct, updateProduct } = require('../controllers/productController');
+const { getAllProducts, addProduct, getProduct, removeProduct, updateProduct, reviewAdd } = require('../controllers/productController');
 const { authCheck, adminCheck } = require('../middlewares/auth_middleware');
 const { fileCheck, updateCheck } = require('../middlewares/file_check');
 const router = express.Router();
@@ -15,8 +15,8 @@ router.route('/').get((req, res) => {
 router.route('/api/products').get(getAllProducts).all(notAllowed);
 router.route('/api/product/:id').get(getProduct).delete(authCheck, adminCheck, removeProduct).patch(authCheck, adminCheck,
   updateCheck, updateProduct).all(notAllowed);
-router.route('/api/add-product').post(authCheck, adminCheck,
-  fileCheck, addProduct).all(notAllowed);
+router.route('/api/add-product').post(authCheck, adminCheck,fileCheck, addProduct).all(notAllowed);
+router.route('/api/add-review/:id').patch(authCheck, reviewAdd).all(notAllowed);
 
 
 
